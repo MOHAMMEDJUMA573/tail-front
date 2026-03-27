8dbbe3ad91ec547df588cdeb00cfeb42c0729cf9
phantomx <mohammedkutunzajuma@gmail.com>
2026-03-27 09:55:44 +0100

    first-commit

---
+++ public/project/js/pro_index.js
@@ -0,0 +1,29 @@
+/*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+const navMenu = document.getElementById("nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", ()=> {
+    navMenu.classList.toggle("left-[0]");
+    hamburger.classList.toggle("ri-close-large-line");
+})
+
+navLinks.forEach((link, index) =>{
+    link.addEventListener("click", ()=>{
+        navMenu.classList.toggle("left-[0]");
+        hamburger.classList.toggle("ri-close-large-line");
+    })
+})
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
